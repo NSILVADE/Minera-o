464cf9790e99ebf4b9643e91b8e362106ef95cbf
NSILVADE <91691102+NSILVADE@users.noreply.github.com>
2022-04-04 17:42:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,6 @@ dataset = dataset.dropna()
 dataset = dataset.reset_index(drop=True)
     
 st.header('Plot dos Dados')
-st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
 
 summarised_results = dataset["sent_rating"].value_counts()
 qtd_star = ('Positiva', 'Negativa')
